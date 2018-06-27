76df2624d6150038a7b72198e90762774d09b5ef
Marcin Krawczyk <MKrawcz3@rockwellautomation.com>
2018-06-27 13:04:55 +0200

    add store devtools enabling timetravelling feature!!

--- application/src/app/app.module.ts
+++ application/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
 // import { reducer } from './reducers/app.reducer';
@@ -14,6 +15,9 @@ import { reducer } from './reducers/todos.reducer';
   imports: [
     BrowserModule,
     StoreModule.forRoot({todos: reducer}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
